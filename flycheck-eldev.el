043545e085ff173d5f1134b9190a3a6517d81f6f
Paul Pogonyshev <pogonyshev@gmail.com>
2020-06-14 21:01:01 +0200

    Add `Eldev' and `Eldev-local' to `auto-mode-alist' on initialization.

--- flycheck-eldev.el
+++ flycheck-eldev.el
@@ -334,6 +334,8 @@ See Info Node `(elisp)Byte Compilation'."
               (when (processp process)
                 (process-put process 'flycheck-working-directory (file-name-directory (buffer-file-name))))
               process))))
+  ;; I don't think we need a separate package just for this, so let's do it here.
+  (add-to-list 'auto-mode-alist `(,(rx "/" (or "Eldev" "Eldev-local") eos) . emacs-lisp-mode) t)
   ;; Deactivate 0.9 hacks.
   (advice-remove 'flycheck-compute-working-directory 'flycheck-eldev--compute-working-directory)
   (advice-remove 'flycheck-start-command-checker     'flycheck-eldev--start-command-checker))
